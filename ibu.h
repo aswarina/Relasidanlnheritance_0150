534f232597fdcde950e9c568232862582896a027
aswarina <lyxsyyawa@gmail.com>
2026-05-12 18:05:41 +0700

    inisialisasi header guard dan deklarasi atribut class ibu

---
+++ ibu.h
@@ -0,0 +1,11 @@
+#ifndef IBU_H
+#define IBU_H
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
