379dba6580e94b0f3a3cfcdfd8d7cf745ca4046e
aswarina <lyxsyyawa@gmail.com>
2026-05-12 19:41:33 +0700

    instansiasi objek manusia untuk mendemonstrasikan komposisi

--- komposisi.cpp
+++ komposisi.cpp
@@ -5,3 +5,13 @@ using namespace std;
 #include "jantung.h"
 #include "manusia.h"
 
+int main() 
+{
+    // Membuat objek manusia di heap memori
+    manusia *varManusia = new manusia("Markus");
+    
+    // Jangan lupa dihapus supaya destructor-nya jalan (biar jantungnya "mati")
+    delete varManusia;
+    
+    return 0;
+}
