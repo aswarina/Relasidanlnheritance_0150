fbb7c80eb91074d3cd4adf394ecbf230238a8b4b
aswarina <lyxsyyawa@gmail.com>
2026-05-12 19:27:54 +0700

    implementasi constructor dan destructor pada class jantung

--- ibu.h
+++ ibu.h
@@ -1,31 +1,24 @@
 #ifndef IBU_H
 #define IBU_H
 #include <vector>
-#include <string>
-#include <iostream>
-using namespace std;
-
+#include "anak.h"
 class ibu {
 public:
     string nama;
     vector<anak*> daftar_anak;
 
-    ibu(string pNama) : nama(pNama) {
+    ibu(string pNama) :nama(pNama) {
         cout << "Ibu \"" << nama << "\" ada\n";
     }
-
     ~ibu() {
         cout << "Ibu \"" << nama << "\" tidak ada\n";
     }
-
     void tambahAnak(anak*);
     void cetakAnak();
 };
-
 void ibu::tambahAnak(anak* pAnak) {
     daftar_anak.push_back(pAnak);
 }
-
 void ibu::cetakAnak() {
     cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
     /*for (auto& a : daftar_anak) {
@@ -36,5 +29,4 @@ void ibu::cetakAnak() {
     }
     cout << endl;
 }
-
 #endif
